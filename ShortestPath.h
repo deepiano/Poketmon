f064e5f159a6a22d8bbca41463feda42bd4d17e5
sleepiano <bong0790@naver.com>
2017-04-08 21:46:49 +0900

    second commit

---
+++ ShortestPath.h
@@ -0,0 +1,32 @@
+#ifndef _SHORTESTPATH_H_
+#define _SHORTESTPATH_H_
+
+#include "GraphType.h"
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+using namespace std;
+
+const int INF = 9999;
+
+struct DistanceContainer
+{
+	int vertex;
+	int distance;
+};
+
+// 우선순위 큐 연산자 overloading
+struct Comp
+{
+	bool operator() (struct DistanceContainer lhs, struct DistanceContainer rhs)
+	{
+		return lhs.distance > rhs.distance;
+	}
+};
+
+
+void Dijkstra(int num_vertex, int** adj, int start, int end, vector<NodeType>& route, int& time);	// 거리 벡터 채우기
+
+#endif
