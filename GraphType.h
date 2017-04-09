75654ffe17eb0c55b40815e3ff85d8467813709b
VleuDive <hot1999130@gmail.com>
2017-04-09 23:02:33 +0900

    Update GraphType.h
    Mapping Table관련 함수 추가, AdjMatrix의 값 설정 함수 추가

--- GraphType.h
+++ GraphType.h
@@ -21,6 +21,7 @@ private:
 	NodeType* nodeList;// �׷����� ���� ��ü ������ ����Ʈ.
 	int** AdjMatrix;// �׷����� ���� ���¸� ��Ÿ���� ��������.
 	int nodeCount;// �׷����� ���� ���� ��.
+	Vector<Vector<int>> mappingTable;
 
 public:
 	GraphType();
@@ -38,6 +39,11 @@ public:
 	NodeType getNodeByIndex(int idx);// �ε����� �޾� �ش� �ε����� ���� ����.
 	int** getAdjMatrix();// �������� ����.
 	int getNodeCount();//���� ���� �� ����.
+	
+	void AddToAdj(int num,int row,int col);// AdjMatrix[row][col]의 값을 num으로 설정. 다른 인접행렬 값 설정 방법을 찾으면 제거 예정.
+    	void AddNodeToMpTable(NodeType nod);// Mapping Table에 노드 추가
+    	void ChangeNodeType(int idx,int type);//nodeList의 idx 인덱스의 타입을 type으로 변경
+    	void MakeMappingTable();//Mapping Table을 만드는 함수로 AddNodeToMpTable을 호출해 사용.
 
 };
 
